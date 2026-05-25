0e21e9780b734cd7bc1555b3de387d7c96423740
Sarah Ferrari <sarahferrarir@gmail.com>
2026-05-25 09:20:33 -0300

    CRUD operations
    Added endpoints for fetching, deleting, and updating students.

--- springboot/src/main/java/com/example/demo/AlunoController.java
+++ springboot/src/main/java/com/example/demo/AlunoController.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/alunos")
-@Tag(name = "Gerenciamento de Alunos", description = "Endpoints para criar e listar alunos")
+@Tag(name = "Gerenciamento de Alunos", description = "")
 public class AlunoController {
 
     private final GerenciadorAlunos gerenciadorAlunos;
@@ -19,12 +20,11 @@ public class AlunoController {
     }
 
     @PostMapping
-    @Operation(summary = "Inserir novo aluno", description = "Adiciona um novo aluno ao banco de dados MySQL.")
-    public String inserirAluno(
+    @Operation(summary = "Inserir novo aluno", description = "Adiciona um novo aluno ao banco.")
+    public void inserirAluno(
             @RequestParam String matricula,
             @RequestParam String nome) {
         gerenciadorAlunos.insercao(matricula, nome);
-        return "Aluno inserido com sucesso!";
     }
 
     @GetMapping
@@ -32,4 +32,34 @@ public class AlunoController {
     public List<Aluno> listarAlunos() {
         return gerenciadorAlunos.resultado();
     }
+
+    @GetMapping("/{matricula}")
+    @Operation(summary = "Buscar aluno por matrícula", description = "Retorna os detalhes de um aluno específico.")
+    public ResponseEntity<Aluno> buscarAluno(@PathVariable String matricula) {
+
+        Aluno aluno = gerenciadorAlunos.buscarPorMatricula(matricula);
+
+        if (aluno != null) {
+            return ResponseEntity.ok(aluno);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Deletar aluno", description = "Deleta um aluno do banco.")
+    public void deletarAluno(
+            @RequestParam String matricula) {
+        gerenciadorAlunos.deletar(matricula);
+    }
+
+    @PutMapping
+    @Operation(summary = "Atualizar aluno", description = "Atualiza o nome de um aluno")
+    public void atualizarAluno(
+            @RequestParam String matricula,
+            @RequestParam String novoNome){
+        gerenciadorAlunos.atualizar(matricula,novoNome);
+    }
+
+
 }
